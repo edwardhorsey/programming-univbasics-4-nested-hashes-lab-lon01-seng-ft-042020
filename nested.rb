f67bc68982685d4fdc8538effbd8467154748100
Edward Horsey <edwardhorsey@me.com>
2020-02-24 13:44:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,7 @@ def adding_matz
         }
      }
 programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LiSP", "C"]}
-    
+return programmer_hash
 end
 
 def changing_alan
